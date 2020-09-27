d12456d42376689dd66210cd0a737643dd067730
DionisiusBagas <dionisiusbagas@gmail.com>
2020-09-27 23:07:22 +0700

    Splash Screen - Bagas

--- app/src/main/java/com/arnawajuan/tubes_uts/MainActivity.java
+++ app/src/main/java/com/arnawajuan/tubes_uts/MainActivity.java
@@ -2,13 +2,26 @@ package com.arnawajuan.tubes_uts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button btnLempar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLempar = findViewById(R.id.buttonLempar);
+
+        btnLempar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SplashScreen.class);
+                startActivity(intent);
+            }
+        });
     }
 }
