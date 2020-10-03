42dfcb0de83094d1598ee18e293e7816d4166971
Arnawa Juan <arnawajuan12@gmail.com>
2020-10-04 01:31:30 +0700

    add main home

--- app/src/main/java/com/arnawajuan/tubes_uts/MainActivity.java
+++ app/src/main/java/com/arnawajuan/tubes_uts/MainActivity.java
@@ -8,21 +8,11 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-Button btnLempar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnLempar = findViewById(R.id.buttonLempar);
-
-        btnLempar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,SplashScreen.class);
-                startActivity(intent);
-
-            }
-        });
     }
 }
